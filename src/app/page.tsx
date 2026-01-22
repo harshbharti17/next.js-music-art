788a74331ed398a0454abf0a7c11fb2d4831df11
HarshBharti17 <harshbharti179@gmail.com>
2026-01-22 20:59:35 +0530

    adding some changes and modifiction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-3xl">Hello NextJS</h1>
-    </>
+    <main className="min-h-screen bg-black/96 antialiased">
+      <HeroSection />
+    </main>
   );
 }
