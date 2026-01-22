b52a844518d69e17a87bdca83ec6a3c37ab0768b
HarshBharti17 <harshbharti179@gmail.com>
2026-01-22 13:12:04 +0530

    testing nextjs are working or not

---
+++ src/app/home/icon/page.tsx
@@ -0,0 +1,7 @@
+export default function IconPage() {
+    return (
+        <>
+        <h1>icon page</h1>
+        </>
+    )
+}
---
+++ src/app/home/layout.tsx
@@ -0,0 +1,14 @@
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <>
+       <h1>inner layout item</h1>
+        {children}
+   </>
+  );
+}
