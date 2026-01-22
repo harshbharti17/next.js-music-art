5c713c917bbdcd3425ed4e49158be89d213204ef
HarshBharti17 <harshbharti179@gmail.com>
2026-01-22 18:42:11 +0530

    customize our layout file and some changes

--- src/app/home/icon/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function IconPage() {
-    return (
-        <>
-        <h1>icon page</h1>
-        </>
-    )
-}
--- src/app/home/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function HomePage(){
-    return(
-        <>
-        <h1 className="text-3xl text-center">Home Page</h1>
-        </>
-    )
-}
