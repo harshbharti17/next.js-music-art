0068bf5c886498d43c5ffa1ed2b7d0e1042fcb0d
HarshBharti17 <harshbharti179@gmail.com>
2026-01-23 21:14:32 +0530

    add featured courses component in main page file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
